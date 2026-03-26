56dbc11caf9060efda02bb57f6649e6a16ec5031
shahpalsha <shahpalsha@gmail.com>
2026-03-26 12:43:52 +0530

    .

--- flutter_circle_timer/lib/src/circle_timer_utils.dart
+++
@@ -1,9 +0,0 @@
-String formatTime(int seconds) {
-  int m = seconds ~/ 60;
-  int s = seconds % 60;
-
-  String mm = m.toString().padLeft(2, '0');
-  String ss = s.toString().padLeft(2, '0');
-
-  return "$mm:$ss";
-}

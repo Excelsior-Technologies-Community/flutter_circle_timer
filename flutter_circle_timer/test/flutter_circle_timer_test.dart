8eccdfe1d3d9958bf03c6cebe546f8e495b4f069
shahpalsha <shahpalsha@gmail.com>
2026-03-26 12:52:16 +0530

    .

--- flutter_circle_timer/test/flutter_circle_timer_test.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-void main() {
-  test('Package loads without error', () {
-    expect(true, true); }); }
--- lib/flutter_circle_timer.dart
+++
@@ -1,5 +0,0 @@
-library flutter_circle_timer;
-
-export 'src/circle_timer.dart';
-export 'src/circle_timer_controller.dart';
-export 'src/circle_timer_style.dart';
